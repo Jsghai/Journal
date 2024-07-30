4c6e17c01040408e543c4bc214c380fcee6d2a94
Jaskaran Singh Ghai <jaskaransinghghai@gmail.com>
2024-07-30 15:18:31 +0300

    Fix 2

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const homeStartingContent = "Hello! Welcome to the Entelyst Journal. Why dont yo
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-mongoose.connect("mongodb://127.0.0.1:27017/fruitsDB", {useNewUrlParser: true});
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
 
 const postS = new mongoose.Schema({
@@ -33,6 +33,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 var posts = [];
+app.use(express.static("public"));
+
 
 app.get("/", function(req, res){
 
@@ -97,10 +99,7 @@ app.get("/clear-posts", function(req, res) {
   });
 });
 
-const sslServer = https.createServer({
-  key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
-  cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
-},app)
-
-
-sslServer.listen(3000, () => console.log("Server Secure on posrt 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
